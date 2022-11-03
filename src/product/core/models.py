23b30bb38d32171b33755842b58c559948c707b9
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2022-11-03 22:10:43 +0700

    update product service

--- src/product/core/models.py
+++ src/product/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(blank=False, null=False, auto_now_add=True)
+    modified_at = models.DateTimeField(blank=False, null=False, auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    class Meta:
+        abstract = True
