9892ebcd573b7b16f76cb39ccc0bdd0c36bb3171
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2022-11-06 21:09:11 +0700

    update product service

--- src/product/product/apps/category/views.py
+++ src/product/product/apps/category/views.py
@@ -4,13 +4,14 @@ from .models import Category
 from .serializers import CategorySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
     def get_queryset(self):
         title = self.request.query_params.get('title')
         parent_id = self.request.query_params.get('parent_id')
 
-        query = Category.objects.all()
+        query = self.queryset
         if title:
             query = query.filter(title=title)
         if parent_id:
