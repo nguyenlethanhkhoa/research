2a754eb451575eacbe24d3b84c550b8a505260f6
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2022-11-06 21:06:31 +0700

    update product service

--- src/product/product/apps/category/views.py
+++ src/product/product/apps/category/views.py
@@ -1,33 +1,24 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from .models import Category
 from .serializers import CategorySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    def list(self, request):
-        title = request.query_params.get('title')
-        parent_id = request.query_params.get('parent_id')
+    def get_queryset(self):
+        title = self.request.query_params.get('title')
+        parent_id = self.request.query_params.get('parent_id')
 
-        query = self.queryset
+        query = Category.objects.all()
         if title:
             query = query.filter(title=title)
         if parent_id:
             query = query.filter(parent_id=parent_id)
 
-        serializer = self.serializer_class(query, many=True)
-        return Response(serializer.data)
+        return query
 
-    def create(self, request):
-        item = Category.objects.create(
-            title=request.data.get('title'),
-            parent_id=request.data.get('parent_id'),
-            description=request.data.get('description')
-        )
+    def perform_create(self):
+        serializer.save(owner=self.request.data)
 
-        item.save()
-        serializer = self.serializer_class(item)
-        return Response(serializer.data)
+
