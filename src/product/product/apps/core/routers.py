fd78f5410a932ca6d34e5b02c070f88a83b39847
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2022-11-07 16:00:49 +0700

    update product service

--- src/product/product/apps/core/routers.py
+++ src/product/product/apps/core/routers.py
@@ -1,3 +1,4 @@
+from django.urls import re_path
 from rest_framework.routers import DefaultRouter, Route, DynamicRoute
 
 
@@ -24,7 +25,7 @@ class CustomRouter(DefaultRouter):
         ),
         # Detail route.
         Route(
-            url=r'^{prefix}/{lookup}/{trailing_slash}$',
+            url=r'^{prefix}/{lookup}{trailing_slash}$',
             mapping={
                 'get': 'retrieve',
                 'put': 'update',
@@ -44,3 +45,48 @@ class CustomRouter(DefaultRouter):
             initkwargs={}
         ),
     ]
+
+    def get_urls(self):
+        """
+        Use the registered viewsets to generate a list of URL patterns.
+        """
+        ret = []
+
+        for prefix, viewset, basename in self.registry:
+            lookup = self.get_lookup_regex(viewset)
+            routes = self.get_routes(viewset)
+
+            for route in routes:
+
+                # Only actions which actually exist on the viewset will be bound
+                mapping = self.get_method_map(viewset, route.mapping)
+                if not mapping:
+                    continue
+
+                # Build the url pattern
+                regex = route.url.format(
+                    prefix=prefix,
+                    lookup=lookup,
+                    trailing_slash=self.trailing_slash
+                )
+
+                print(regex)
+
+                # If there is no prefix, the first part of the url is probably
+                #   controlled by project's urls.py and the router is in an app,
+                #   so a slash in the beginning will (A) cause Django to give
+                #   warnings and (B) generate URLS that will require using '//'.
+                if not prefix and regex[:2] == '^/':
+                    regex = '^' + regex[2:]
+
+                initkwargs = route.initkwargs.copy()
+                initkwargs.update({
+                    'basename': basename,
+                    'detail': route.detail,
+                })
+
+                view = viewset.as_view(mapping, **initkwargs)
+                name = route.name.format(basename=basename)
+                ret.append(re_path(regex, view, name=name))
+
+        return ret
