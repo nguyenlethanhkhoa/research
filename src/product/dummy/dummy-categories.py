6fd29c0229a06e1d873c6ad97bd9f8042cb77d2f
Khoa Nguyen <khoa.nguyenlethanh@gmail.com>
2022-11-09 15:49:06 +0700

    update product service

---
+++ src/product/dummy/dummy-categories.py
@@ -0,0 +1,36 @@
+import requests
+
+
+resp = requests.get('https://shopee.vn/api/v4/pages/get_category_tree')
+resp = resp.json()
+categories = resp.get('data').get('category_list')
+
+no = 0
+for category in categories:
+    no += 1
+    print('Add ' + category.get('display_name'))
+    requests.post(
+        'http://128.199.232.249:10000/product/categories',
+        json={
+            "title": category.get('name'),
+            "parent_id": category.get('parent_catid'),
+            'display_name': category.get('display_name'),
+            'level': category.get('level')
+        }
+    )
+
+    for sub_category in category.get('children'):
+        no += 1
+        print('Add ' + sub_category.get('display_name'))
+        requests.post(
+            'http://128.199.232.249:10000/product/categories',
+            json={
+                "title": sub_category.get('name'),
+                "parent_id": sub_category.get('parent_catid'),
+                'display_name': sub_category.get('display_name'),
+                'level': sub_category.get('level')
+            }
+        )
+
+print(f'Done with {no} items')
+
